5ceb9564858b4f871c5531d1cb407072ad1e72b6
Łukasz Adamczyk <lukaszadamczyk@mbp-ukasz.home>
2024-03-06 19:39:08 +0000

    added cv interface and random user

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+interface Experience {
+  year: number;
+  description: string;
+}
+
+interface CVData {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string;
+  experience: Experience[];
+  education: string[];
+}
+
+const cvData: CVData = {
+  photo: '',
+  name: 'Jan',
+  lastName: 'Kowalski',
+  position: 'Web Developer',
+  experience: [
+  {
+    year: 2020,
+    description: 'Google'
+  },
+  {
+    year: 2022,
+    description: 'Facebook'
+  }
+],
+  education: ['Oxford'],
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello world!</h1>
+    <h1>{cvData.education}</h1>
   </React.StrictMode>,
 )
